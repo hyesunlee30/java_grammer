f62bc457377cf4bca6c41ad247f52fb15fca7848
hyesunlee30 <sunnylee3771@gmail.com>
2023-12-05 17:26:10 +0900

    논리연산자

--- src/C04Operater.java
+++ src/C04Operater.java
@@ -27,34 +27,55 @@ public class C04Operater {
 //        n4 /= 3;
 //        n5 %= 3;
 //        System.out.println(n4+" "+n5);
+//
+//        //증감연산자
+//        //전위 증감 연산자
+//        // 현재 라인의 명령문이 실행되기 전에 값이 증감
+//        // ++a
+//        // --a
+//        //후위 증감 연산자
+//        // 현재 라인의 명령문이 끝나고 나서 증감.
+//        // a++
+//        // a--
+//
+//        int a = 5;
+//        int b = a++; // 현재 라인의 명령문이 끝나고 나서 증감.
+//        System.out.println(a);//6
+//        System.out.println(b);//5
+//
+//        a=5;
+//        b=++a; // 현재 라인의 명령문이 끝나기 전에 증감.
+//        System.out.println(a); //6
+//        System.out.println(b); //6
+//
+//        int [] arr = {10,20,30};
+//        a=0;
+//        while (true) {
+//            a++; // 증가시키고 검색
+//            System.out.println(arr[a]);
+//            a++;// 검색하고 증가
+//        }
+//
+//        // 비교연산자
+//
+//        char ch1 = 'a';
+//        char ch2 = 'A';
+//
+//        System.out.println(ch1 == ch2); // 같다
+//        System.out.println(ch1 != ch2); // 같지 않다.
 
-        //증감연산자
-        //전위 증감 연산자
-        // 현재 라인의 명령문이 실행되기 전에 값이 증감
-        // ++a
-        // --a
-        //후위 증감 연산자
-        // 현재 라인의 명령문이 끝나고 나서 증감.
-        // a++
-        // a--
+        // 논리연산자 참, 거짓 &&, ||, !
+        int num1 = 10; int num2 = 20;
+        boolean result1, result2;
 
-        int a = 5;
-        int b = a++; // 현재 라인의 명령문이 끝나고 나서 증감.
-        System.out.println(a);//6
-        System.out.println(b);//5
+        result1 = num1 > 5 && num1 < 20; // 둘 다 만족해서 true
+        result2 = num2 < 10 || num2 < 30; //둘 중에 하나만이라도 만족해서 true
 
-        a=5;
-        b=++a; // 현재 라인의 명령문이 끝나기 전에 증감.
-        System.out.println(a); //6
-        System.out.println(b); //6
+        System.out.println(result1); // ture
+        System.out.println(result2); // ture
+        System.out.println(!result2); // ture의 반대 false
 
-        int [] arr = {10,20,30};
-        a=0;
-        while (true) {
-            a++; // 증가시키고 검색
-            System.out.println(arr[a]);
-            a++;// 검색하고 증가
-        }
+
 
 
 
